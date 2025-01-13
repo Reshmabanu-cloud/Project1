2ffb5becf52133a632a2ce7f03b4cdb4e0be3394
Reshma banu <reshmanbanu786@gmail.com>
2025-01-13 17:07:27 +0530

    first commit

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,5 +1,14 @@
 public class Lab1 {
+    static int b=20;
+    int c =30;
     public static void main(String[] args) {
+Lab1 l = new Lab1();
+        int a = 10;
+
+
         System.out.println("hi");
+        System.out.println(a);
+        System.out.println(Lab1.b);
+        System.out.println(l.c);
     }
 }
